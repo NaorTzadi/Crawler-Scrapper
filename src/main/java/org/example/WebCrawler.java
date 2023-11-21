47419648fddae65c5bd91f6645f21c8e88ba49ab
NaorTzadi <118543436+NaorTzadi@users.noreply.github.com>
2023-11-21 23:37:50 +0200

    Update WebCrawler.java

--- src/main/java/org/example/WebCrawler.java
+++ src/main/java/org/example/WebCrawler.java
@@ -4,12 +4,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
 import java.util.ArrayList;
 public class WebCrawler {
-
-    public static WebCrawlerData getPageLinks(String URL) {
+    public static WebCrawlerData getUrlInfo(String url){
+        WebCrawlerData webCrawlerData=new WebCrawlerData(-1,-1, new ArrayList<>(),null);
+        getPageLinks(url,webCrawlerData);
+        String language = getLanguageFromHTML(url);
+        webCrawlerData.setLanguage(language);
+        return webCrawlerData;
+    }
+    private static void getPageLinks(String URL,WebCrawlerData webCrawlerData) {
         ArrayList<String> links = new ArrayList<>();
-        WebCrawlerData webCrawlerData=null;
         try {
             Document document = Jsoup.connect(URL).get();
             Elements linksOnPage = document.select("a[href]");
@@ -21,17 +28,46 @@ public class WebCrawler {
                     links.add(link);
                 }
             }
-            webCrawlerData=new WebCrawlerData(counter,links.size(),links);
+            webCrawlerData.setIterationsCount(counter);
+            webCrawlerData.setUniqueLinksCount(links.size());
+            webCrawlerData.setLinksList(links);
             System.out.println(webCrawlerData);
 
-
-        } catch (IOException e) {
-            System.err.println("For '" + URL + "': " + e.getMessage());
-        }
-        return webCrawlerData;
+        } catch (IOException e) {System.err.println("For '" + URL + "': " + e.getMessage());}
     }
-
-
-
+    private static String getLanguageFromHTML(String url) {
+        try {
+            Document document = Jsoup.connect(url).get();
+            Element htmlTag = document.select("html").first();
+            return htmlTag.attr("lang"); // Returns the value of the lang attribute
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private static String extractDomainInfo(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            String host = url.getHost();
+            String[] parts = host.split("\\.");
+            String domain = parts[parts.length - 2] + "." + parts[parts.length - 1];
+            String subdomain = (parts.length > 2) ? parts[0] : "";
+            return "Domain: " + domain + ", Subdomain: " + subdomain;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private static String getServerLocation(String url) {
+        try {
+            InetAddress address = InetAddress.getByName(new URL(url).getHost());
+            String ip = address.getHostAddress();
+            // Here, use an IP geolocation API to get the location from the IP
+            return "IP: " + ip + ", Location: [Use API to get location]";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
