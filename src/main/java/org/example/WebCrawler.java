8795964805731e904176a4e366b8cd5f97ecdfb9
NaorTzadi <118543436+NaorTzadi@users.noreply.github.com>
2023-11-27 12:45:06 +0200

    Update WebCrawler.java

--- src/main/java/org/example/WebCrawler.java
+++ src/main/java/org/example/WebCrawler.java
@@ -1,21 +1,28 @@
 package org.example;
+import org.json.JSONObject;
 import org.jsoup.nodes.Element;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.ArrayList;
 public class WebCrawler {
-    public static WebCrawlerData getUrlInfo(String url){
-        WebCrawlerData webCrawlerData=new WebCrawlerData(-1,-1, new ArrayList<>(),null);
-        getPageLinks(url,webCrawlerData);
-        String language = getLanguageFromHTML(url);
-        webCrawlerData.setLanguage(language);
+
+    public static WebCrawlerDataBase getUrlInfo(String url){
+        WebCrawlerDataBase webCrawlerData=new WebCrawlerDataBase(url,-1,-1, new ArrayList<>(),null,null,null);
+
+        setPageLinksInfo(url,webCrawlerData);
+        webCrawlerData.setLanguage(getLanguageFromHTML(url));
+        webCrawlerData.setUrlIPAddress(getIPAddressFromUrl(url));
+        webCrawlerData.setLocation(getServerLocationFromIPAddress(webCrawlerData.getUrlIPAddress()));
         return webCrawlerData;
     }
-    private static void getPageLinks(String URL,WebCrawlerData webCrawlerData) {
+    private static void setPageLinksInfo(String URL, WebCrawlerDataBase webCrawlerData) {
         ArrayList<String> links = new ArrayList<>();
         try {
             Document document = Jsoup.connect(URL).get();
@@ -31,8 +38,6 @@ public class WebCrawler {
             webCrawlerData.setIterationsCount(counter);
             webCrawlerData.setUniqueLinksCount(links.size());
             webCrawlerData.setLinksList(links);
-            System.out.println(webCrawlerData);
-
         } catch (IOException e) {System.err.println("For '" + URL + "': " + e.getMessage());}
     }
     private static String getLanguageFromHTML(String url) {
@@ -45,29 +50,53 @@ public class WebCrawler {
             return null;
         }
     }
-    private static String extractDomainInfo(String urlString) {
-        try {
-            URL url = new URL(urlString);
-            String host = url.getHost();
-            String[] parts = host.split("\\.");
-            String domain = parts[parts.length - 2] + "." + parts[parts.length - 1];
-            String subdomain = (parts.length > 2) ? parts[0] : "";
-            return "Domain: " + domain + ", Subdomain: " + subdomain;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    private static String getServerLocation(String url) {
+    private static String getIPAddressFromUrl(String url){
+        String ip;
         try {
             InetAddress address = InetAddress.getByName(new URL(url).getHost());
-            String ip = address.getHostAddress();
-            // Here, use an IP geolocation API to get the location from the IP
-            return "IP: " + ip + ", Location: [Use API to get location]";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+            ip = address.getHostAddress();
+        }catch (Exception e) {e.printStackTrace();return null;}
+        return ip;
     }
+    public static String getServerLocationFromIPAddress(String ip) {
+        //לצורך חידוש מפתח: https://ipgeolocation.io/documentation/ip-geolocation-api.html
+        final String APIKey="2ce1e6fb6b484fd4a39a431ecd66ae9f";
+        String info;
+        try {
+           String requestUrl = "https://api.ipgeolocation.io/ipgeo?apiKey=" + APIKey + "&ip=" + ip;
+           info=extractGeoLocationBasicInformation(getResponseFromURL(requestUrl));
+        } catch (Exception e) {e.printStackTrace();return null;}
+        return info;
+    }
+    private static String getResponseFromURL(String urlString) {
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {e.printStackTrace();}
+        return response.toString();
+    }
+    private static String extractGeoLocationBasicInformation(String info){
+        JSONObject jsonObject = new JSONObject(info);
+        String continent = jsonObject.getString("continent_name");
+        String country = jsonObject.getString("country_name");
+        String district = jsonObject.optString("district", "N/A"); // Using optString for optional fields
+        String city = jsonObject.getString("city");
+        String zipCode = jsonObject.getString("zipcode");
+        String latitude = jsonObject.getString("latitude");
+        String longitude = jsonObject.getString("longitude");
+        return "Continent: " + continent + ", Country: " + country +
+                ", District: " + district + ", City: " + city +
+                ", Zip Code: " + zipCode + ", Latitude: " + latitude +
+                ", Longitude: " + longitude;
+    }
+
 
 }
