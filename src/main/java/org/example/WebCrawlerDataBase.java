91dd807cfb644182e6e709ba7ee29265c62c2840
NaorTzadi <118543436+NaorTzadi@users.noreply.github.com>
2024-01-04 19:24:39 +0200

    Update WebCrawlerDataBase.java

--- src/main/java/org/example/WebCrawlerDataBase.java
+++ src/main/java/org/example/WebCrawlerDataBase.java
@@ -4,15 +4,15 @@ public class WebCrawlerDataBase {
     private String url;
     private int iterationsCount;
     private int uniqueLinksCount;
-    private ArrayList<String> linksList;
+    private ArrayList<String> uniqueLinksList;//i think it suppose to be uniqueLinksList which is also a set.
     private String language;
     private String urlIPAddress;
     private String location;
-    public WebCrawlerDataBase(String url,int iterationsCount, int uniqueLinksCount, ArrayList<String> linksList, String language, String urlIPAddress, String location) {
+    public WebCrawlerDataBase(String url, int iterationsCount, int uniqueLinksCount, ArrayList<String> uniqueLinksList, String language, String urlIPAddress, String location) {
         this.url=url;
         this.iterationsCount = iterationsCount;
         this.uniqueLinksCount = uniqueLinksCount;
-        this.linksList = linksList;
+        this.uniqueLinksList = uniqueLinksList;
         this.language=language;
         this.urlIPAddress=urlIPAddress;
         this.location=location;
@@ -20,14 +20,14 @@ public class WebCrawlerDataBase {
     public String getUrl(){return url;}
     public int getIterationsCount() {return iterationsCount;}
     public int getUniqueLinksCount() {return uniqueLinksCount;}
-    public ArrayList<String> getLinksList() {return linksList;}
+    public ArrayList<String> getUniqueLinksList() {return uniqueLinksList;}
     public String getLanguage(){return language;}
     public String getUrlIPAddress(){return urlIPAddress;}
     public String getLocation(){return location;}
     public void setUrl(String url){this.url=url;}
     public void setIterationsCount(int iterationsCount) {this.iterationsCount = iterationsCount;}
     public void setUniqueLinksCount(int uniqueLinksCount) {this.uniqueLinksCount = uniqueLinksCount;}
-    public void setLinksList(ArrayList<String> linksList) {this.linksList = linksList;}
+    public void setUniqueLinksList(ArrayList<String> uniqueLinksList) {this.uniqueLinksList = uniqueLinksList;}
     public void setLanguage(String language){this.language=language;}
     public void setUrlIPAddress(String urlIPAddress){this.urlIPAddress=urlIPAddress;}
     public void setLocation(String location){this.location=location;}
@@ -38,7 +38,7 @@ public class WebCrawlerDataBase {
                 "\n url='" + url + '\'' +
                         ",\n iterationsCount=" + iterationsCount +
                         ",\n uniqueLinksCount=" + uniqueLinksCount +
-                        ",\n linksList=" + linksList +
+                        ",\n linksList=" + uniqueLinksList +
                         ",\n language='" + language + '\'' +
                         ",\n urlIPAddress='" + urlIPAddress + '\'' +
                         ",\n location='" + location + '\'' +
